16ddbc3d6ba0a796cb780b403c52cd370bdf9f11
Kaique Sacchi <sacchikaique@gmail.com>
2021-02-24 16:45:38 -0300

    Added Apollo setup.

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<React.StrictMode></React.StrictMode>, document.getElementById("root"));
+import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <div></div>
+  </ApolloProvider>,
+  document.getElementById("root")
+);
